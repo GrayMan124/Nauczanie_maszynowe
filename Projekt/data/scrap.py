23f6999da11c6b6e2a3232024a4c03f63c99a4cb
Antoni Czernek <80044736+GrayMan124@users.noreply.github.com>
2021-12-11 16:36:39 +0100

    Project Scrapping Reuters

--- Projekt/data/scrap.py
+++ Projekt/data/scrap.py
@@ -2,31 +2,39 @@
 import re
 import requests
 import itertools
+import time
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 
 
-df=pd.DataFrame(columns=['Date','Headline'])
-
-print(df)
-
+df=pd.read_csv('headlines_reuters')
+df=df.drop("Unnamed: 0",axis=1)
 headline_regex = re.compile('h3 class="story-title"')
 
 url='https://www.reuters.com/news/archive/eurMktRpt?view=page&page=1&pageSize=10'
-response = requests.get(url)
+response = requests.get(url,timeout=5)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
 headlines = soup.find('body').find_all('h3','class'=='story-title')
-time = soup.find('body').find_all('time')
+time1 = soup.find('body').find_all('time')
 
+l=0
 
-
-for i in range(len(time)):
-
-    df.loc[i]=[time[i].text.strip()]+[headlines[i].text.strip()]
+for k in range(1,1438):
+    time.sleep(5)
+    url='https://www.reuters.com/news/archive/eurMktRpt?view=page&page='+str(k)+'&pageSize=10'
+    response = requests.get(url,timeout=30)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    headlines = soup.find('body').find_all('h3','class'=='story-title')
+    time1 = soup.find('body').find_all('time')
+    print("Jestem na stronie: "+ str(k))
+    print("Skocz se po kawke, to troche zajmie")
+    for i in range(len(time1)):
+        df.loc[l]=[time1[i].text.strip()]+[headlines[i].text.strip()]
+        l+=1
 
 
 print(df)
-df.to_csv('headlines')
+df.to_csv('headlines_reuters')
